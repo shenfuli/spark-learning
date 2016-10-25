037719b8de3bb2b611dddd5d1d801d47950025e4
fuli.shen <fuli.shen@9icaishi.net>
2016-10-25 11:56:54 +0800

    commit

---
+++ src/main/java/org/training/spark/JHelloWorld.java
@@ -0,0 +1,12 @@
+package org.training.spark;
+
+/**
+ * Created by fuli.shen on 2016/10/25.
+ */
+public class JHelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+
+}
---
+++ src/main/scala/org/training/spark/HelloWorld.scala
@@ -0,0 +1,11 @@
+package org.training.spark
+
+/**
+  * Created by fuli.shen on 2016/10/25.
+  */
+object HelloWorld {
+
+  def main(args: Array[String]) {
+    print("Hello World");
+  }
+}
