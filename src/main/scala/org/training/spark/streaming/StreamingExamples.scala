9f4b693348001a39f426171356dece9a471702ec
沈福利 <fuli.shen@9icaishi.net>
2017-05-19 17:41:12 +0800

    add streaming networkwordcount case

---
+++ src/main/scala/org/training/spark/streaming/StreamingExamples.scala
@@ -0,0 +1,20 @@
+package org.training.spark.streaming
+
+import org.apache.log4j.{Level, Logger}
+
+/**
+  * Created by fuli.shen on 2017/5/19.
+  */
+object StreamingExamples {
+  /** Set reasonable logging levels for streaming if the user has not configured log4j. */
+  def setStreamingLogLevels() {
+    val log4jInitialized = Logger.getRootLogger.getAllAppenders.hasMoreElements
+    if (!log4jInitialized) {
+      // We first log something to initialize Spark's default logging, then we override the
+      // logging level.
+      Logger.getRootLogger.info("Setting log level to [WARN] for streaming example." +
+        " To override add a custom log4j.properties to the classpath.")
+      Logger.getRootLogger.setLevel(Level.WARN)
+    }
+  }
+}
